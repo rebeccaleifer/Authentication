8b2e9efe84ca2a3c5c97518e559726bfb47f7e68
rebeccaleifer <rebecca.leifer15@gmail.com>
2022-04-07 18:43:43 -0600

    Changed Password Requirements

--- Startup.cs
+++ Startup.cs
@@ -77,9 +77,21 @@ namespace Authentication
                 new InferenceSession("wwwroot/utah_crash_severity.onnx")
             );
 
-            //THIS IS A TEST
+            //MFA
             services.AddAuthorization(options =>
                 options.AddPolicy("TwoFactorEnabled", x => x.RequireClaim("amr", "mfa")));
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Set Password Requirements
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 8;
+                options.Password.RequiredUniqueChars = 1;
+            });
+
         }
         // NEW STUFF (I HOPE THIS WORKS)
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
