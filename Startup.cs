da604f7106a6408c221fb91bc7d4b2ad4264a318
rebeccaleifer <rebecca.leifer15@gmail.com>
2022-04-07 22:56:21 -0600

    Fixed a problem

--- Startup.cs
+++ Startup.cs
@@ -29,10 +29,7 @@ namespace Authentication
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
-            //services.AddDbContext<ApplicationDbContext>(options =>
-            //    options.UseSqlServer(
-            //        Configuration.GetConnectionString("DefaultConnection")));
+            //services.AddMvc();
             
             services.AddDefaultIdentity<IdentityUser>(options =>
                 options.SignIn.RequireConfirmedAccount = true)
@@ -60,12 +57,6 @@ namespace Authentication
                 options.UseMySql(Configuration.GetConnectionString("ApplicationDataDbConnection"))
             );
 
-            //services.AddIdentity<IdentityUser, IdentityRole>()
-            //    .AddEntityFrameworkStores<ApplicationDbContext>();
-
-            //services.AddDbContext<ApplicationDbContext>(options =>
-            //    options.UseMySql(Configuration["ConnectionStrings: ApplicationDataDbConnection"]));
-
             services.AddControllersWithViews();
             //services.AddRazorPages();
 
@@ -83,10 +74,6 @@ namespace Authentication
             services.AddSingleton<InferenceSession>(
                 new InferenceSession("wwwroot/utah_crash_severity.onnx")
             );
-
-            //MFA
-            services.AddAuthorization(options =>
-                options.AddPolicy("TwoFactorEnabled", x => x.RequireClaim("amr", "mfa")));
 
             services.Configure<IdentityOptions>(options =>
             {
@@ -125,8 +112,6 @@ namespace Authentication
             app.UseAuthentication();
             app.UseAuthorization();
 
-            //app.UseMvcWithDefaultRoute();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
@@ -153,39 +138,3 @@ namespace Authentication
         }
     }
 }
-
-
-
-// OLD STUFF
-// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-//        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-//        {
-//            if (env.IsDevelopment())
-//            {
-//                app.UseDeveloperExceptionPage();
-//                app.UseDatabaseErrorPage();
-//            }
-//            else
-//            {
-//                app.UseExceptionHandler("/Home/Error");
-//                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//                app.UseHsts();
-//            }
-//            app.UseHttpsRedirection();
-//            app.UseStaticFiles();
-
-//            app.UseRouting();
-
-//            app.UseAuthentication();
-//            app.UseAuthorization();
-
-//            app.UseEndpoints(endpoints =>
-//            {
-//                endpoints.MapControllerRoute(
-//                    name: "default",
-//                    pattern: "{controller=Home}/{action=Index}/{id?}");
-//                endpoints.MapRazorPages();
-//            });
-//        }
-//    }
-//}
